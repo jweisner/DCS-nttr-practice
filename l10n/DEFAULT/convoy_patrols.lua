69698bb0ff19dcf5b6e9adf63d5f879662f13407
Jesse Weisner <jesse@weisner.ca>
2016-09-02 14:55:29 -0700

    Added more targets, removed red players
    - removed all Red coalition player units. Map is now blue coop only
    - added convoys and tanks to Alamo
    - added super-easy fuel truck targets in Boulder
    - added Ka-50 FARP in Moapa
    - added road blocks and tanks for Moapa Ka-50s

--- l10n/DEFAULT/convoy_patrols.lua
+++ l10n/DEFAULT/convoy_patrols.lua
@@ -1,5 +1,14 @@
 -- set patrols
 mist.ground.patrol('Medium Convoy #002', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #001', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #002', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #003', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #004', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #005', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #006', 'doubleBack')
+mist.ground.patrol('Alamo Convoy #007', 'doubleBack')
+mist.ground.patrol('Caliente Alpha Convoy #001', 'doubleBack')
+mist.ground.patrol('Caliente Bravo Convoy #001', 'doubleBack')
 
 -- group dead flags
 --[[
@@ -9,7 +18,22 @@ Flags:
  1011 - Medium Convoy #001 Dead
  1012 - Medium Convoy #002 Dead
  1021 - Tank Convoy #001 Dead
-]]
+ 2001 - Alamo Convoy #001
+ 2002 - Alamo Convoy #002
+ 2003 - Alamo Convoy #003
+ 2004 - Alamo Convoy #004
+ 2005 - Alamo Convoy #005
+ 2006 - Alamo Convoy #006
+ 2007 - Alamo Convoy #007
+ 2101 - Caliente Alpha Convoy #001
+ 2102 - Caliente Bravo Convoy #001
+ 2201 - Mormon Mesa T-72 #001
+ 2202 - Mormon Mesa T-72 #002
+ 2203 - Mormon Mesa T-72 #003
+ 2204 - Mormon Mesa T-72 #004
+ 2205 - Mormon Mesa T-72 #005
+ 2206 - Mormon Mesa T-72 #006
+ ]]
 mist.flagFunc.group_dead {
 	groupName = 'Easy Convoy #001',
 	flag = 1001,
@@ -38,5 +62,95 @@ mist.flagFunc.group_dead {
 mist.flagFunc.group_dead {
 	groupName = 'Tank Convoy #001',
 	flag = 1021,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #001',
+	flag = 2001,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #002',
+	flag = 2002,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #003',
+	flag = 2003,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #004',
+	flag = 2004,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #005',
+	flag = 2005,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #006',
+	flag = 2006,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Alamo Convoy #007',
+	flag = 2007,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Caliente Alpha Convoy #001',
+	flag = 2101,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Caliente Bravo Convoy #001',
+	flag = 2102,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Mormon Mesa T-72 #001',
+	flag = 2201,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Mormon Mesa T-72 #002',
+	flag = 2202,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Mormon Mesa T-72 #003',
+	flag = 2203,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Mormon Mesa T-72 #004',
+	flag = 2204,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Mormon Mesa T-72 #005',
+	flag = 2205,
+	toggle = true,
+}
+
+mist.flagFunc.group_dead {
+	groupName = 'Mormon Mesa T-72 #006',
+	flag = 2206,
 	toggle = true,
 }
